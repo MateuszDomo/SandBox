0fc7f74c816676f2121d8a8f4a8f2ec6cea6cdf7
Mateusz <matpdomo@gmail.com>
2023-04-04 20:18:57 -0700

    Converting 2d array of int points to 2 seperate arrays of floats. This will simulate grains of sand on a cartesian plane

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-from sand_box import SandBox
+from sand_box import SandBox, BoxBounds
+from matplotlib.animation import FuncAnimation
+
 import matplotlib.pyplot as plt
 
 # TODO: make each grain of sand have x,y and paint them. In order to apply wave functions on them.
 def main():
-    sand_box: SandBox = SandBox(1000,1000)
-    window = plt.imshow(sand_box.box, cmap='Greys')
-    plt.pause(2)
-    sand_box.fillSandBox()
-    window.set_data(sand_box.box)
-    plt.draw()
-    plt.pause(5)
-    plt.clf()
+    
+    sandbox = SandBox(100,BoxBounds(100.0,100.0,0.0,0.0))
+
+    fig, ax = plt.subplots(figsize=(6,6), facecolor = 'white',frameon=False)
+    fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
+
+    ax.set_axis_off()
+    def update(frame):
+        sandbox.update()
+        ax.clear()
+        ax.scatter(sandbox.grainsX,sandbox.grainsY,s=3)
+    
+    anim = FuncAnimation(fig,update,frames=100,interval=16.67)
+
+    plt.show()
+
+def update(ax,sandbox,frame):
+    sandbox.update()
+    ax.clear()
+    ax.scatter(sandbox.grainsX,sandbox.grainsY,s=3)
 
 if __name__ == "__main__":
     main()
--- sand_box.py
+++ sand_box.py
@@ -1,13 +1,54 @@
 import random
 
-class SandBox:
-    def __init__(self, rows: int, cols: int): 
-        self.box = [[0 for c in range(cols)] for r in range(rows)]
-        self.rows = rows
-        self.cols = cols
-        self.fillSandBox()
+class BoxBounds:
+    def __init__(self, top: float, right: float, bottom: float, left: float):
+        self.top: float = top
+        self.right: float = right
+        self.bottom: float = bottom
+        self.left: float = left
 
-    def fillSandBox(self):
-        for r in range(self.rows):
-            for c in range(self.cols):
-                self.box[r][c] = random.choice([0,1])
+class XY:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+    
+    def update(self, x: int, y: int):
+        self.x = x
+        self.y = y
+        
+class SandBox:
+    def __init__(self, num_grains: int, box_bounds: BoxBounds): 
+        self.num: int = num_grains
+        self.grainsX: list = []
+        self.grainsY: list = []
+        self.bbox: BoxBounds = box_bounds
+        self._fillSandBox()
+
+    def _fillSandBox(self):
+        top = self.bbox.top
+        right = self.bbox.right
+        bottom = self.bbox.bottom
+        left = self.bbox.left
+    
+        for i in range(self.num):
+            self.grainsX.append(random.uniform(left,right))
+            self.grainsY.append(random.uniform(bottom,top))
+
+    def update(self):
+        top = self.bbox.top
+        right = self.bbox.right
+        bottom = self.bbox.bottom
+        left = self.bbox.left
+        displacement = 1
+
+        for i in range(self.num):
+            dx = random.uniform(-displacement, displacement)
+            dy = random.uniform(-displacement, displacement)
+            self.grainsX[i] += dx
+            self.grainsY[i] += dy
+
+            # make sure the grains stay within the bounding box
+            self.grainsX[i] = max(self.grainsX[i], left)
+            self.grainsX[i] = min(self.grainsX[i], right)
+            self.grainsY[i] = max(self.grainsY[i], bottom)
+            self.grainsY[i] = min(self.grainsY[i], top)
