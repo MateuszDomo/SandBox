682afc2455a8f27f77043f820502fd920597d0d5
Mateusz <matpdomo@gmail.com>
2023-04-07 12:13:05 -0700

    Integrated numpy to increase update(frame) speed by over %25.2. This optimization improves fps

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 from sand_box import SandBox, BoxBounds
 from matplotlib.animation import FuncAnimation
 
+import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 # TODO: make each grain of sand have x,y and paint them. In order to apply wave functions on them.
 def main():
     
-    sandbox = SandBox(100,BoxBounds(100.0,100.0,0.0,0.0))
-
+    sandbox = SandBox(1000,BoxBounds(100.0,100.0,0.0,0.0))
     fig, ax = plt.subplots(figsize=(6,6), facecolor = 'white',frameon=False)
     fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
-
     ax.set_axis_off()
+
+    ax.scatter(sandbox.grainsX,sandbox.grainsY,s=3)
+
     def update(frame):
         sandbox.update()
         ax.clear()
-        ax.scatter(sandbox.grainsX,sandbox.grainsY,s=3)
-    
-    anim = FuncAnimation(fig,update,frames=100,interval=16.67)
+        scatter = ax.scatter(sandbox.grainsX,sandbox.grainsY,s=3)
+        return scatter,
+    anim = FuncAnimation(fig,update,frames = 100, interval =16.67, blit = True)
 
     plt.show()
 
-def update(ax,sandbox,frame):
-    sandbox.update()
-    ax.clear()
-    ax.scatter(sandbox.grainsX,sandbox.grainsY,s=3)
 
 if __name__ == "__main__":
     main()
--- sand_box.py
+++ sand_box.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 class BoxBounds:
     def __init__(self, top: float, right: float, bottom: float, left: float):
@@ -7,48 +8,22 @@ class BoxBounds:
         self.bottom: float = bottom
         self.left: float = left
 
-class XY:
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
-    
-    def update(self, x: int, y: int):
-        self.x = x
-        self.y = y
         
 class SandBox:
     def __init__(self, num_grains: int, box_bounds: BoxBounds): 
         self.num: int = num_grains
-        self.grainsX: list = []
-        self.grainsY: list = []
+        self.grainsX: list = np.random.uniform(box_bounds.left,box_bounds.right, size = num_grains)
+        self.grainsY: list = np.random.uniform(box_bounds.bottom,box_bounds.top, size = num_grains)
         self.bbox: BoxBounds = box_bounds
-        self._fillSandBox()
-
-    def _fillSandBox(self):
-        top = self.bbox.top
-        right = self.bbox.right
-        bottom = self.bbox.bottom
-        left = self.bbox.left
-    
-        for i in range(self.num):
-            self.grainsX.append(random.uniform(left,right))
-            self.grainsY.append(random.uniform(bottom,top))
 
     def update(self):
-        top = self.bbox.top
-        right = self.bbox.right
-        bottom = self.bbox.bottom
-        left = self.bbox.left
-        displacement = 1
+        displacement = 3.0
 
-        for i in range(self.num):
-            dx = random.uniform(-displacement, displacement)
-            dy = random.uniform(-displacement, displacement)
-            self.grainsX[i] += dx
-            self.grainsY[i] += dy
+        dx = np.random.uniform(-displacement, displacement, size=self.num)
+        dy = np.random.uniform(-displacement, displacement, size=self.num)
+        self.grainsX += dx
+        self.grainsY += dy
 
-            # make sure the grains stay within the bounding box
-            self.grainsX[i] = max(self.grainsX[i], left)
-            self.grainsX[i] = min(self.grainsX[i], right)
-            self.grainsY[i] = max(self.grainsY[i], bottom)
-            self.grainsY[i] = min(self.grainsY[i], top)
+        # make sure the grains stay within the bounding box
+        np.clip(self.grainsX, self.bbox.left, self.bbox.right, out=self.grainsX)
+        np.clip(self.grainsY, self.bbox.bottom, self.bbox.top, out=self.grainsY)
